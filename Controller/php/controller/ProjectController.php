00d9c4306cd62a2ae511c2806a04942a5f713dcb
lolKami <51096807+lolKami@users.noreply.github.com>
2019-06-06 16:47:00 -0500

    Add files via upload

--- Controller/php/controller/ProjectController.php
+++ Controller/php/controller/ProjectController.php
@@ -1,10 +1,24 @@
 <?php  
+$x = "";
 include '../data/NewProjectPDO.php';
 if ($_POST['action'] === 'create') {
 
 	register($_POST);
 }
- function register($pos){
+if ($_POST['action'] === 'select') {
+	select();
+}
+if ($_POST['action'] === 'Remove') {
+	Remove($_POST);
+}
+if ($_POST['action'] === 'ShowData') {
+	ShowData($_POST);
+}
+if ($_POST['action'] === 'Edit') {
+	Edit($_POST);
+}
+
+ 	function register($pos){
         $ObjectProject = new Project();
         $ObjectProject->setPROJECT_NAME($pos['PROJECT_NAME']);
         $ObjectProject->setCOST($pos['COST']);
@@ -13,4 +27,29 @@ if ($_POST['action'] === 'create') {
        
         return NewProjectPDO::register($ObjectProject);
     }
+
+    function select(){      
+    	return NewProjectPDO::select();
+    }
+	 function Remove($pos){
+	 	$ObjectProject = new Project();
+	 	$ObjectProject->setID($pos['ID']);
+
+	 	return NewProjectPDO::Remove($ObjectProject);
+ 	}
+ 	function ShowData($pos){
+ 		$ObjectProject = new Project();
+ 		$ObjectProject->setID($pos['ID']);
+ 		return NewProjectPDO::ShowData($ObjectProject);
+ 	}
+ 	function Edit($pos){
+ 		$ObjectProject = new Project();
+ 		$ObjectProject->setID($pos['ID']);
+ 		$ObjectProject->setPROJECT_NAME($pos['PROJECT_NAME']);
+ 		$ObjectProject->setCOST($pos['COST']);
+ 		$ObjectProject->setCREATION_DATE($pos['CREATION_DATE']);
+        $ObjectProject->setEND_DATE($pos['END_DATE']);
+        return NewProjectPDO::EditProject($ObjectProject);
+ 	}
+ 	
 ?>
